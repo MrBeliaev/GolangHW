bfd66ea691442c889783cb61c51fe66aa59a03a5
MrBeliaev <mr.beliaev88@mail.ru>
2022-09-08 16:34:51 +0300

    HW2 is completed fix

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -11,32 +11,44 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(str string) (string, error) {
-	if len(str) == 0 {
+	switch len(str) {
+	case 0:
 		return "", nil
-	} else if unicode.IsDigit(rune(str[0])) {
+	case 1:
+		if !unicode.IsDigit(rune(str[0])) {
+			return str, nil
+		}
 		return "", ErrInvalidString
-	} else if len(str) == 1 {
-		return str, nil
+	default:
+		if unicode.IsDigit(rune(str[0])) {
+			return "", ErrInvalidString
+		}
 	}
 
 	var newStr strings.Builder
 
 	for i := 1; i < len(str); i++ {
-		if unicode.IsDigit(rune(str[i])) && unicode.IsDigit(rune(str[i-1])) {
-			return "", ErrInvalidString
-		} else if unicode.IsDigit(rune(str[i])) && !unicode.IsDigit(rune(str[i-1])) {
-			count, err := strconv.Atoi(string(str[i]))
-			if err != nil {
-				fmt.Println(err)
+		switch unicode.IsDigit(rune(str[i])) {
+		case true:
+			switch unicode.IsDigit(rune(str[i-1])) {
+			case true:
+				return "", ErrInvalidString
+			case false:
+				count, err := strconv.Atoi(string(str[i]))
+				if err != nil {
+					fmt.Println(err)
+				}
+				if count == 0 {
+					continue
+				}
+				char := strings.Repeat(string(str[i-1]), count)
+				newStr.WriteString(char)
 			}
-			if count == 0 {
-				continue
+		case false:
+			if !unicode.IsDigit(rune(str[i-1])) {
+				char := strings.Repeat(string(str[i-1]), 1)
+				newStr.WriteString(char)
 			}
-			char := strings.Repeat(string(str[i-1]), count)
-			newStr.WriteString(char)
-		} else if !unicode.IsDigit(rune(str[i-1])) {
-			char := strings.Repeat(string(str[i-1]), 1)
-			newStr.WriteString(char)
 		}
 	}
 	if !unicode.IsDigit(rune(str[len(str)-1])) {
