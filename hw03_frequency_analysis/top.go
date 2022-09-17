01159c3ace85ecb04b128573bb7ae8477dfcf70f
MrBeliaev <mr.beliaev88@mail.ru>
2022-09-17 16:10:22 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,40 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+var splitRegex = regexp.MustCompile(`\s+`)
+
+func Top10(str string) []string {
+	if strings.TrimSpace(str) == "" {
+		return nil
+	}
+	strArr := splitRegex.Split(str, -1)
+	wordsCount := map[string]int{}
+	var maxCount int
+	for _, v := range strArr {
+		wordsCount[v]++
+		if maxCount < wordsCount[v] {
+			maxCount = wordsCount[v]
+		}
+	}
+	result := make([]string, 0)
+	for i := maxCount; i > 0; i-- {
+		arr := make([]string, 0)
+		for word, count := range wordsCount {
+			if i == count {
+				arr = append(arr, word)
+			}
+		}
+		sort.Strings(arr)
+		result = append(result, arr...)
+		arr = nil
+	}
+	if len(result) >= 10 {
+		return result[:10]
+	}
+	return result
 }
